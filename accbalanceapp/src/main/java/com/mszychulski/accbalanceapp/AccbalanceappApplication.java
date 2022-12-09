4dcdec7b482237ad477420525468dd562e144d6c
MichaelSzychulski <michael.szychulski@gmail.com>
2022-12-09 12:29:09 +0100

    Initial configuration

---
+++ accbalanceapp/src/main/java/com/mszychulski/accbalanceapp/AccbalanceappApplication.java
@@ -0,0 +1,13 @@
+package com.mszychulski.accbalanceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccbalanceappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccbalanceappApplication.class, args);
+	}
+
+}
---
+++ accbalanceapp/src/test/java/com/mszychulski/accbalanceapp/AccbalanceappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mszychulski.accbalanceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccbalanceappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
